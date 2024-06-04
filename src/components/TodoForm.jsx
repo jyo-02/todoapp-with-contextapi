bc94c34204d09bbdefc1d09f6935de042f365168
jyo <bosejyotiska@gmail.com>
2024-06-04 12:37:00 +0530

    add comment lines

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import { useTodo } from '../context/TodoContext'
 
 function TodoForm() {
-    const [todo, setTodo] = useState("")
+    //first we bring the context useTodo hook
     const {addTodo} = useTodo()
+    const [todo, setTodo] = useState("")
+    
 
     const add = (e) => {
       e.preventDefault()
@@ -11,7 +13,7 @@ function TodoForm() {
       if (!todo) return
 
       addTodo({ todo, completed: false})
-      setTodo("")
+      setTodo("") // making the textfield empty by passing empty string to state
     }
 
   return (
@@ -20,8 +22,8 @@ function TodoForm() {
               type="text"
               placeholder="Write Todo..."
               className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
-              value={todo}
-              onChange={(e) => setTodo(e.target.value)}
+              value={todo} //wiring with the state variable
+              onChange={(e) => setTodo(e.target.value)} // if there's any change, we send it to state using setTodo
           />
           <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
               Add
